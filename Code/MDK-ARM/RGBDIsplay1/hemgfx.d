a5210acc7ac88c08f367b85856129c82e0b8ffce
RisingRedstone <57984380+RisingRedstone@users.noreply.github.com>
2020-04-21 19:50:41 +0530

    Add files via upload

---
+++ Code/MDK-ARM/RGBDIsplay1/hemgfx.d
@@ -0,0 +1,5 @@
+rgbdisplay1\hemgfx.o: HemGfx.cpp
+rgbdisplay1\hemgfx.o: E:\Application\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+rgbdisplay1\hemgfx.o: E:\Application\Keil\ARM\ARMCC\Bin\..\include\stdlib.h
+rgbdisplay1\hemgfx.o: HemGfx.h
+rgbdisplay1\hemgfx.o: ../Src/Bitmaps.c
---
+++ Code/MDK-ARM/RGBDIsplay1/hemgfxwrapper.d
@@ -0,0 +1,6 @@
+rgbdisplay1\hemgfxwrapper.o: HemGfxWrapper.cpp
+rgbdisplay1\hemgfxwrapper.o: HemGfxWrapper.h
+rgbdisplay1\hemgfxwrapper.o: E:\Application\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+rgbdisplay1\hemgfxwrapper.o: E:\Application\Keil\ARM\ARMCC\Bin\..\include\stdbool.h
+rgbdisplay1\hemgfxwrapper.o: HemGfx.h
+rgbdisplay1\hemgfxwrapper.o: E:\Application\Keil\ARM\ARMCC\Bin\..\include\stdlib.h
